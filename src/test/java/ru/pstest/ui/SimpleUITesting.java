62960896fde24eeb6142f88a3484216d80c277dc
psolokha <pavel.solokha@gmail.com>
2024-01-15 15:33:00 +0300

    allure reporter added

--- src/test/java/ru/pstest/ui/SimpleUITesting.java
+++ src/test/java/ru/pstest/ui/SimpleUITesting.java
@@ -1,14 +1,12 @@
 package ru.pstest.ui;
 
 import com.codeborne.selenide.selector.ByText;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 public class SimpleUITesting {
 
-    @Disabled
     @Test
     public void testSimpleUIText() {
         open("https://www.google.ru/");
