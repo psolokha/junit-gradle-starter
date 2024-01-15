8f0f0abe69724cbba4e27b985933152677bbebf1
psolokha <pavel.solokha@gmail.com>
2024-01-15 15:11:07 +0300

    disabled ui test

--- src/test/java/ru/pstest/ui/SimpleUITesting.java
+++ src/test/java/ru/pstest/ui/SimpleUITesting.java
@@ -1,12 +1,14 @@
 package ru.pstest.ui;
 
 import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 public class SimpleUITesting {
 
+    @Disabled
     @Test
     public void testSimpleUIText() {
         open("https://www.google.ru/");
